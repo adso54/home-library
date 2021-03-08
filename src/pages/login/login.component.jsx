fe61b19f04223c308823848e3b3fa438dbe08beb
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2021-03-08 06:56:22 +0100

    Add reset pasword option in login page.

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Form, Col, Button} from 'react-bootstrap'
-
+import {Link} from 'react-router-dom'
 import './login.styles.scss';
 
 class LogIn extends React.Component{
@@ -8,7 +8,9 @@ class LogIn extends React.Component{
         super()
         this.state= {
             email: '',
-            password: ''
+            password: '',
+            passwordReset: false,
+            passwordResetEmailSend: false
         }
     }
 
@@ -17,39 +19,80 @@ class LogIn extends React.Component{
         this.setState({[name]: value})
     }
 
+    handleResetPassword = (e) =>{
+        this.setState({passwordReset: true})
+    }
+
+    passwordReset = (email) => {
+        this.setState({passwordResetEmailSend: true})
+
+        fetch(process.env.REACT_APP_SERV_ADRESS + '/user/passwordReset', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: email,
+                appLink: process.env.REACT_APP_SERV_ADRESS + '/user/changePassword/'
+            })
+        })
+    }
+
     render(){
         return(
            <div className="logIn">
                <div className="logInForm">
-                   <Form>
-                       <h1 className="title">
-                           Sign In
-                       </h1>
-                        <Form.Group as={Col} controlId="formGridTitle" >
-                            <Form.Label className='label' >Email</Form.Label>
-                            <Form.Control 
-                                type="text" 
-                                placeholder="Email" 
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                                name="email"
-                            />
-                            <Form.Label className='label' >Password</Form.Label>
-                            <Form.Control 
-                                type="password" 
-                                placeholder="Password" 
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                name="password"
-                            />
-                        </Form.Group>
-                        <div className="logInButton">
-                            <Button 
-                                ariant="success" 
-                                className="but" 
-                                onClick={()=> this.props.userSignIn(this.state.email, this.state.password)}>Sign In</Button>
-                        </div>
-                   </Form>
+                   {this.state.passwordResetEmailSend === false ?
+                    <Form>
+                        <h1 className="title">
+                            {this.state.passwordReset === false ? 'Sign In': 'Reset password' }
+                        </h1>
+                            <Form.Group as={Col} controlId="formGridTitle" >
+                                <Form.Label className='label' >Email</Form.Label>
+                                <Form.Control 
+                                    type="text" 
+                                    placeholder="Email" 
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
+                                    name="email"
+                                />
+                                {this.state.passwordReset === false ?
+                                    <div>
+                                        <Form.Label className='label' >Password</Form.Label>
+                                        <Form.Control 
+                                            type="password" 
+                                            placeholder="Password" 
+                                            value={this.state.password}
+                                            onChange={this.handleChange}
+                                            name="password"
+                                        />
+                                        <Link to='#' className="navbar-link"  onClick={this.handleResetPassword}>
+                                            Forgot password
+                                        </Link> 
+                                    </div>
+                                : null
+                                }
+                            </Form.Group>
+                        
+                            <div className="logInButton">
+                            {this.state.passwordReset === false ? 
+                                <Button 
+                                    variant="success" 
+                                    className="but" 
+                                    onClick={()=> this.props.userSignIn(this.state.email, this.state.password)}>Sign In 
+                                </Button>
+                            : 
+                                <Button 
+                                    variant="success" 
+                                    className="but" 
+                                    onClick={()=> this.passwordReset(this.state.email)}>Password reset 
+                                </Button>
+                            }
+                            </div>
+                    </Form>
+                   :
+                    <h1 className="title">
+                        Check your email for reset password!
+                    </h1>
+                   }
                </div>
            </div>
         )
